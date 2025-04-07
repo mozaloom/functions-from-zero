dc0e320a67ade5ebb56b3f4351d200b973b3b1af
Mohammed Zaloom <125036726+mozaloom@users.noreply.github.com>
2025-04-07 13:21:21 +0000

    add cli tool and create mylib

--- test_wikibot.py
+++ test_wikibot.py
@@ -1,4 +1,14 @@
-from wikibot import scrape
+from mylib.bot import scrape
+from click.testing import CliRunner
+from wikibot import cli
 
 def test_scrape():
     assert "Microsoft" in scrape("Microsoft")
+
+#test the cli
+
+def test_cli():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--name', 'Microsoft'])
+    assert result.exit_code == 0
+    assert "Summary of Microsoft:" in result.output
--- wikibot.py
+++ wikibot.py
@@ -1,8 +1,16 @@
 import wikipedia
+from mylib.bot import scrape
+import click
+
+@click.command()
+@click.option('--name', prompt='Wikipedia page name',
+              help='The name of the Wikipedia page to scrape.')
+@click.option('--length', default=1, help='Number of sentences to scrape.')
+
+def cli(name="Microsoft", length=1):
+    result = scrape(name, length=length)           
+    click.echo(click.style(f"Summary of {name}: {result}",bg='white', fg='green'))
 
 
-def scrape(name="Microsoft", length=1):
-    result = wikipedia.summary(name, sentences=length)
-    return result
-
-print(scrape())
+if __name__ == '__main__':
+    cli()
