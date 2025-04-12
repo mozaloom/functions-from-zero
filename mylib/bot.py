73becc1cb29714df1c98e46ff669c5c918c79682
Mohammed Zaloom <125036726+mozaloom@users.noreply.github.com>
2025-04-12 11:18:22 +0000

    modification

--- mylib/bot.py
+++ mylib/bot.py
@@ -1,5 +1,23 @@
 import wikipedia
+from yake import KeywordExtractor
 
 def scrape(name="Microsoft", length=1):
     result = wikipedia.summary(name, sentences=length)
-    return result           
+    return result     
+
+def search(name="Microsoft"):
+    result = wikipedia.search(name)
+    return result
+    
+
+def get_wiki_page(name="Microsoft"):
+    page = wikipedia.page(name)
+    return page.content
+
+#return the top 10 keywords of the wikipedia page
+def get_keywords(name="Microsoft"):
+    extractor = KeywordExtractor()
+    keywords = extractor.extract_keywords(get_wiki_page(name))
+    return keywords[:10]
+
+
